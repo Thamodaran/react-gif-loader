2f359a829e45abd2910c5dd8b129147ab33dc852
thamodarank <thamodaran@ideas2it.com>
2018-12-05 17:22:53 +0530

    First commit with base

---
+++ README.md
@@ -0,0 +1,39 @@
+# React Gif Loader
+## Install
+```
+npm install react-gif-loader --save
+
+```
+
+## Demo
+<img src="https://github.com/Thamodaran/react-gif-loader/raw/master/demo.gif" alt="demo" width="100%" />
+
+## Usage
+```
+React gif loader example with customize your own styles
+```
+```js
+import GifLoader from 'react-gif-loader';
+
+class ReactGifLoader extends Component {
+    render() {
+        return (
+            <GifLoader
+                loading={true}
+                imageSrc="https://media.giphy.com/media/l378zKVk7Eh3yHoJi/source.gif"
+                imageStyle={imageStyle}
+                overlayBackground="rgba(0,0,0,0.5"
+            />
+        );
+    }
+}
+```
+
+## PROPTYPES
+| Prop | Type | Default | Required |
+| ---- | ---- | ------- | ---------|
+| imageSrc | String | "https://media.giphy.com/media/l378zKVk7Eh3yHoJi/source.gif" | No |
+| overlayBackground | String | "rgba(0,0,0,0.4)" | No |
+| imageStyle | Object | {marginTop: "20%"} | No |
+| loading | Boolean | - | Yes |
+# react-gif-loader
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+class GifLoader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+  render() {
+      const {imageSrc, imageStyle, overlayBackground, loading} = this.props;
+      const overlayBlockStyle = {
+        zIndex: "99999",
+        height: "100vh",
+        width: "100%",
+        position: "absolute",
+        display: loading ? "block" : "none",
+        backgroundColor: overlayBackground
+    }
+    return (
+      <div style={overlayBlockStyle} >
+          <img style={imageStyle} src={imageSrc} />
+      </div>
+    );
+  }
+}
+GifLoader.propTypes = {
+    imageSrc: PropTypes.string,
+    overlayBackground: PropTypes.string,
+    imageStyle: PropTypes.object,
+    loading: PropTypes.bool.isRequired,
+  };
+GifLoader.defaultProps = {
+    imageSrc: "https://media.giphy.com/media/l378zKVk7Eh3yHoJi/source.gif",
+    imageStyle: {marginTop: "20%"},
+    overlayBackground: "rgba(0,0,0,0.4)"
+};
+export default GifLoader;
